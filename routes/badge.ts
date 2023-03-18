2ee10e6152f134fd326759e67b8dd6fc5e84b42d
SaintKappa <00wojtekz@gmail.com>
2023-03-19 00:24:12 +0100

    badge fix

--- routes/badge.ts
+++ routes/badge.ts
@@ -10,7 +10,7 @@ router.get('/:userName', async (req, res) => {
         const pushEvent: any = data.find((event: any) => event.type === 'PushEvent' && event.repo.name.split('/')[1] !== req.params.userName);
 
         const badge = `
-        <svg fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg fill="none" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
           <foreignObject width="100%" height="100%">
             <div xmlns="http://www.w3.org/1999/xhtml">
               <span>I was last seen working on <a href="https://github.com/${pushEvent?.repo.name}" target="_blank">${pushEvent?.repo.name.split('/')[1]}</a></span>
