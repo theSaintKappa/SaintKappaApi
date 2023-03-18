a28c3a368840193e1e7a12b60e6042ea393a2a9d
SaintKappa <00wojtekz@gmail.com>
2023-03-19 00:29:46 +0100

    🪦 Content-Type header fix

--- routes/badge.ts
+++ routes/badge.ts
@@ -3,6 +3,7 @@ const router = express.Router();
 import fetch from 'node-fetch';
 
 router.get('/:userName', async (req, res) => {
+    res.setHeader('Content-Type', 'image/svg+xml');
     try {
         const response = await fetch(`https://api.github.com/users/${req.params.userName}/events`);
         const data = (await response.json()) as object[];
@@ -10,7 +11,7 @@ router.get('/:userName', async (req, res) => {
         const pushEvent: any = data.find((event: any) => event.type === 'PushEvent' && event.repo.name.split('/')[1] !== req.params.userName);
 
         const badge = `
-        <svg fill="none" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+        <svg fill="none" xmlns="http://www.w3.org/2000/svg">
           <foreignObject width="100%" height="100%">
             <div xmlns="http://www.w3.org/1999/xhtml">
               <span>I was last seen working on <a href="https://github.com/${pushEvent?.repo.name}" target="_blank">${pushEvent?.repo.name.split('/')[1]}</a></span>
@@ -18,9 +19,9 @@ router.get('/:userName', async (req, res) => {
           </foreignObject>
         </svg>`;
 
-        res.status(200).send(badge);
+        res.send(badge);
     } catch (err) {
-        res.status(500).send(`<svg fill="none" xmlns="http://www.w3.org/2000/svg"><foreignObject width="100%" height="100%"><div xmlns="http://www.w3.org/1999/xhtml"><span>Badge failed to load</span></div></foreignObject></svg>`);
+        res.send(`<svg fill="none" xmlns="http://www.w3.org/2000/svg"></svg>`);
     }
 });
 
