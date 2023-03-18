0ce84a3bb94e6626d4bb25f45b7104048894bcf4
SaintKappa <00wojtekz@gmail.com>
2023-03-19 00:11:29 +0100

    svg fix

--- routes/badge.ts
+++ routes/badge.ts
@@ -9,9 +9,11 @@ router.get('/:userName', async (req, res) => {
 
         const pushEvent: any = data.find((event: any) => event.type === 'PushEvent' && event.repo.name.split('/')[1] !== req.params.userName);
 
-        res.status(200).send(`<svg fill="none" xmlns="http://www.w3.org/2000/svg">I was last seen working on <a href="${pushEvent?.repo.url}" target="_blank">${pushEvent?.repo.name.split('/')[1]}</a></svg>`);
+        res.status(200).send(
+            `<svg fill="none" xmlns="http://www.w3.org/2000/svg"><foreignObject width="100%" height="100%">I was last seen working on <a href="${pushEvent?.repo.url}" target="_blank">${pushEvent?.repo.name.split('/')[1]}</a></foreignObject></svg>`
+        );
     } catch (err) {
-        res.status(500).send(`<svg fill="none" xmlns="http://www.w3.org/2000/svg">Badge failed to load</svg>`);
+        res.status(500).send(`<svg fill="none" xmlns="http://www.w3.org/2000/svg"><foreignObject width="100%" height="100%">Badge failed to load</foreignObject></svg>`);
     }
 });
 
