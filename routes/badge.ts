f1dbe618e4f19e92c9730e6d480f9fd4ee2fe120
SaintKappa <00wojtekz@gmail.com>
2023-03-19 00:19:20 +0100

    svg fix

--- routes/badge.ts
+++ routes/badge.ts
@@ -9,11 +9,18 @@ router.get('/:userName', async (req, res) => {
 
         const pushEvent: any = data.find((event: any) => event.type === 'PushEvent' && event.repo.name.split('/')[1] !== req.params.userName);
 
-        res.status(200).send(
-            `<svg fill="none" xmlns="http://www.w3.org/2000/svg"><foreignObject width="100%" height="100%">I was last seen working on <a href="${pushEvent?.repo.url}" target="_blank">${pushEvent?.repo.name.split('/')[1]}</a></foreignObject></svg>`
-        );
+        const badge = `
+        <svg fill="none" xmlns="http://www.w3.org/2000/svg">
+          <foreignObject width="100%" height="100%">
+            <div xmlns="http://www.w3.org/1999/xhtml">
+              <span>I was last seen working on <a href="https://github.com/${pushEvent?.repo.name}" target="_blank">${pushEvent?.repo.name.split('/')[1]}</a></span>
+            </div>
+          </foreignObject>
+        </svg>`;
+
+        res.status(200).send(badge);
     } catch (err) {
-        res.status(500).send(`<svg fill="none" xmlns="http://www.w3.org/2000/svg"><foreignObject width="100%" height="100%">Badge failed to load</foreignObject></svg>`);
+        res.status(500).send(`<svg fill="none" xmlns="http://www.w3.org/2000/svg"><foreignObject width="100%" height="100%"><div xmlns="http://www.w3.org/1999/xhtml"><span>Badge failed to load</span></div></foreignObject></svg>`);
     }
 });
 
